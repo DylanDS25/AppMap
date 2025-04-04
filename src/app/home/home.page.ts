72a88ff11a045f7a715fa32da438437735b51888
Dylan <dylanleonardo11@gmail.com>
2025-04-04 00:19:07 -0500

    appMap 2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component } from '@angular/core';
 import * as L from 'leaflet';
- 
+import { Geolocation } from '@capacitor/geolocation';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,34 +9,42 @@ import * as L from 'leaflet';
   standalone: false,
 })
 export class HomePage implements AfterViewInit {
-  private map !: L.Map;
+  private map!: L.Map;
 
-  constructor() {}
- 
+  constructor(private _geolocation: Geolocation) { }
+
   ngAfterViewInit(): void {
     this.initMap();
   }
- 
-  initMap(): void {
 
-    var customIcon = L.
+  async initMap(): Promise<void> {
 
-    this.map = L.map('mapId').setView([4.606449, -74.08132], 18);
+    const position = await Geolocation.getCurrentPosition();
+    const { latitude, longitude } = position.coords;
+    console.log('Current position:', latitude, longitude);
+    const customIcon = L.icon({
+      iconUrl: 'https://static.thenounproject.com/png/7748759-512.png',
+      iconSize: [40, 40],
+      iconAnchor: [20, 40],
+      popupAnchor: [0, -40]
+    });
 
-    L.tileLayer('https://{s}.title.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy: OpenStreetMap contributors'
+    this.map = L.map('mapId').setView([latitude, longitude], 18);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
     }).addTo(this.map);
 
-    L.marker([4.606449, -74.08132])
+    L.marker([latitude, longitude], { icon: customIcon })
       .addTo(this.map)
-      .bindPopup('ETITC')
+      .bindPopup('Estoy aquí!')
       .openPopup();
   }
 
-  ngOnDestroy(): void{
-    if(this.map){
+  ngOnDestroy(): void {
+    if (this.map) {
       this.map.remove();
     }
   }
- 
+
 }
